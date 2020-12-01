f70134f0e1827aae8103bbaa08bc56400aaadbad
DMesure <dmesure98@gmail.com>
2020-12-01 22:43:22 +0100

    Auth0 implemented, navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Router>
+          <Route exact path="/" component={Home}/>
+          <Route path="/profile" component={Profile}/>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
+
+
+export default function Header() {
+    const {isAuthenticated} = useAuth0();
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">FamilyFroots</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#features">Recipes</Nav.Link>
+                    <Nav.Link href="#pricing">About</Nav.Link>
+                </Nav>
+                <Nav>
+                    {!isAuthenticated ? <LoginButton/> : <LogoutButton/>}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
