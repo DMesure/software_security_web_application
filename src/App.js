845bfdd4019d56c5c50c654ecca4da8819c88a7d
DMesure <dmesure98@gmail.com>
2020-12-30 10:52:05 +0100

    env editted

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import Profile from './pages/Profile';
 import Recipes from './pages/Recipes';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 function App() {
   return (
-    <>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={process.env.REACT_APP_AUTH0_CALLBACK_URI}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+      scope="profile email openid"
+    >
       <Header />
+
       <Router>
-          <Route exact path="/" component={Home}/>
-          <Route path="/profile" component={Profile}/>
-          <Route path="/recipes" component={Recipes}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/recipes" component={Recipes} />
       </Router>
-    </>
+    </Auth0Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from "@auth0/auth0-react";
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={process.env.REACT_APP_AUTH0_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={window.location.origin}
-    audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-    scope="profile email openid"
-    >
-    <App />
-  </Auth0Provider>,
+
+  <App />
+  ,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
